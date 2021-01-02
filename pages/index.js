10733ec3ce104d04bc51e756d037730603948878
Douglas Azevedo <douglaaa.s@hotmail.com>
2021-01-02 10:54:19 -0300

    integração cep-promise

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,43 @@
+import { useState }  from 'react'
+import * as cep from 'cep-promise'
+
 function Home() {
-    return <div>Home 2</div>
+    const [cepNumber, setCepNumber] = useState(0);
+    
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        await useCepPromise(cepNumber);
+
+        setCepNumber('');
+    }
+
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="cepNumber">Informe o cep</label>
+                <input name="cepNumber"
+                required
+                value={cepNumber}
+                onChange={e => setCepNumber(e.target.value)}/>
+            </div>
+
+            <button type="submit">Buscar</button>
+
+        </form>
+
+    )
+}
+
+function useCepPromise() {
+    cep(7864000)
+        .then((result) => {
+            console.log(result);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
 }
 
 export default Home;
